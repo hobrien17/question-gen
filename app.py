f992f04537c344983fc714db67069917dbe2adb1
Henry O'Brien <h.obrien17@hotmail.com>
2018-03-31 17:38:38 +1000

    Now outputs json

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from genexp import gen_exp
 
 app = Flask(__name__)
 
 @app.route('/exp')
 def execute_gen_exp():
-    return {"a": 1}
+    return jsonify({"a": 1})
 
 if __name__ == "__main__":
     app.run()
